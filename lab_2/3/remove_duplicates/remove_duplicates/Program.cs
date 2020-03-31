0673049f7311fe4d43b003a4ac0e639eecb9a9c3
Ayar Tokoev <56004530+IThinkthisisKazan@users.noreply.github.com>
2020-03-31 18:00:17 +0300

    Complete

---
+++ lab_2/3/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!\nUsage remove_duplicates.exe <input string>");
+            }
+
+            else
+            {
+                string sourceStr = args[0];
+                string resultStr = new string(sourceStr.Distinct().ToArray());
+                Console.Write(resultStr);
+
+               
+            }
+        }
+    }
+}
